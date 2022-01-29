f40ccd969b790941d75c67749a757010bf1d5ff0
Gualtiero <gualtiero.frigerio@gmail.com>
2022-01-29 18:24:25 +0100

    first commit

---
+++ Sources/ScreenshotSwiftUI/ScreenshotMaker.swift
@@ -0,0 +1,21 @@
+//
+//  ScreenshotMaker.swift
+//
+//
+//  Created by Gualtiero Frigerio on 29/01/22.
+//
+
+import UIKit
+
+public class ScreenshotMaker: UIView {
+    /// Takes the screenshot of the superview of this superview
+    /// - Returns: The UIImage with the screenshot of the view
+    func screenshot() -> UIImage? {
+        guard let containerView = self.superview?.superview,
+              let containerSuperview = containerView.superview else { return nil }
+        let renderer = UIGraphicsImageRenderer(bounds: containerView.frame)
+        return renderer.image { (context) in
+            containerSuperview.layer.render(in: context.cgContext)
+        }
+    }
+}
---
+++ Sources/ScreenshotSwiftUI/ScreenshotMakerView.swift
@@ -0,0 +1,43 @@
+//
+//  ScreenshotMakerView.swift
+//
+//
+//  Created by Gualtiero Frigerio on 29/01/22.
+//
+
+import SwiftUI
+import UIKit
+
+/// This View is needed to create a UIViewRepresentable with a ScreenshotMaker
+public struct ScreenshotMakerView: UIViewRepresentable {
+    let closure: (ScreenshotMaker) -> Void
+    
+    public init(_ closure: @escaping (ScreenshotMaker) -> Void) {
+        self.closure = closure
+    }
+    
+    public func makeUIView(context: Context) -> ScreenshotMaker {
+        let view = ScreenshotMaker(frame: CGRect.zero)
+        return view
+    }
+    
+    public func updateUIView(_ uiView: ScreenshotMaker, context: Context) {
+        DispatchQueue.main.async {
+            closure(uiView)
+        }
+    }
+}
+
+extension View {
+    /// Add to the View you need to take a screenshot of.
+    /// It creates a ScreenshotMakerView and returns a closure with
+    /// a ScreenshotMaker object you can use to take a screenshot
+    /// The view is added in an overlay with allowHitTesting false so it doesn't interfere
+    /// with your View
+    /// - Parameter closure: A closure with a ScreenshotMaker object
+    /// - Returns: an overlay containing a ScreenshotMakerView
+    func screenshotMaker(_ closure: @escaping (ScreenshotMaker) -> Void) -> some View {
+        let screenshotView = ScreenshotMakerView(closure)
+        return overlay(screenshotView.allowsHitTesting(false))
+    }
+}
