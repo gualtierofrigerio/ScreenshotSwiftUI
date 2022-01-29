ffc76a28f04167eecc01e4e96c7e2a4f6c39d9ab
Gualtiero <gualtiero.frigerio@gmail.com>
2022-01-29 18:31:40 +0100

    screenshot made public

--- Sources/ScreenshotSwiftUI/ScreenshotMaker.swift
+++ Sources/ScreenshotSwiftUI/ScreenshotMaker.swift
@@ -10,7 +10,7 @@ import UIKit
 public class ScreenshotMaker: UIView {
     /// Takes the screenshot of the superview of this superview
     /// - Returns: The UIImage with the screenshot of the view
-    func screenshot() -> UIImage? {
+    public func screenshot() -> UIImage? {
         guard let containerView = self.superview?.superview,
               let containerSuperview = containerView.superview else { return nil }
         let renderer = UIGraphicsImageRenderer(bounds: containerView.frame)
