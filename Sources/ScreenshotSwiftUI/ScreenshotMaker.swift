499a4ee91285757d7d186fc52c2db583ffe1c3a7
Gualtiero <gualtiero.frigerio@gmail.com>
2022-01-30 16:45:30 +0100

    ScreenshotMaker now a protocol

--- Sources/ScreenshotSwiftUI/ScreenshotMaker.swift
+++ Sources/ScreenshotSwiftUI/ScreenshotMaker.swift
@@ -7,15 +7,8 @@
 
 import UIKit
 
-public class ScreenshotMaker: UIView {
-    /// Takes the screenshot of the superview of this superview
+public protocol ScreenshotMaker {
+    // Takes the screenshot of the superview of this superview
     /// - Returns: The UIImage with the screenshot of the view
-    public func screenshot() -> UIImage? {
-        guard let containerView = self.superview?.superview,
-              let containerSuperview = containerView.superview else { return nil }
-        let renderer = UIGraphicsImageRenderer(bounds: containerView.frame)
-        return renderer.image { (context) in
-            containerSuperview.layer.render(in: context.cgContext)
-        }
-    }
+    func screenshot() -> UIImage?
 }
